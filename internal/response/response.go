4230f6bf93bde9acd789bbf2e4843402ad9fb784
sunilpar <sunilformal42@gmail.com>
2025-05-05 08:38:30 +0000

    made response package to send correct response

---
+++ internal/response/response.go
@@ -0,0 +1,50 @@
+package response
+
+import (
+	"fmt"
+	"httpfromtcp/internal/headers"
+	"io"
+)
+
+type StatusCode int
+
+const (
+	StatusOK                  StatusCode = 200
+	StatusBadRequest          StatusCode = 400
+	StatusInternalServerError StatusCode = 500
+)
+
+func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+	var reason string
+	switch statusCode {
+	case StatusOK:
+		reason = "OK"
+	case StatusBadRequest:
+		reason = "Bad Request"
+	case StatusInternalServerError:
+		reason = "Internal Server Error"
+	default:
+		reason = ""
+	}
+	_, err := fmt.Fprintf(w, "HTTP/1.1 %d %s\r\n", statusCode, reason)
+	return err
+}
+
+func GetDefaultHeaders(contentLen int) headers.Headers {
+	h := headers.New()
+	h.Set("Content-Length", fmt.Sprintf("%d", contentLen))
+	h.Set("Connection", "close")
+	h.Set("Content-Type", "text/plain")
+	return h
+}
+
+func WriteHeaders(w io.Writer, h headers.Headers) error {
+	for key, value := range h {
+		_, err := fmt.Fprintf(w, "%s: %s\r\n", key, value)
+		if err != nil {
+			return err
+		}
+	}
+	_, err := fmt.Fprint(w, "\r\n")
+	return err
+}
